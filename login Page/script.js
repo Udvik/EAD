ce47cdccd3264500a9aea95197bea36bfb656dc2
Udvik <116430868+Udvik@users.noreply.github.com>
2024-11-28 06:23:52 +0530

    Add files via upload

---
+++ login Page/script.js
@@ -0,0 +1,10 @@
+function submit(){
+const username = document.getElementById('username').value;
+const password = document.getElementById('password').value;
+
+if(username == 'ADMIN' && password == '12345'){
+    alert('Login Successful');
+}else{
+    alert('Invalid Credentials');
+}
+};
